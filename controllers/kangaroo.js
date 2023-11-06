01a62c50b72cb9e6f4f9c64502bc930bdc49c0df
Manideep4444 <manideepg200@gmail.com>
2023-11-05 22:34:52 -0600

    part5

--- controllers/kangaroo.js
+++ controllers/kangaroo.js
@@ -1,8 +1,22 @@
 var kangaroo = require('../models/kangaroo');
 // List of all kangaroo
+
+// List of all Costumes
+exports.kangaroo_list = async function(req, res) {
+try{
+    thekangaroos = await kangaroo.find();
+    res.send(thekangaroos);
+}
+catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+}
+};
+    
+/*
 exports.kangaroo_list = function(req, res) {
 res.send('NOT IMPLEMENTED: kangaroo list');
-};
+};*/
 // for a specific kangaroo.
 exports.kangaroo_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: kangaroo detail: ' + req.params.id);
