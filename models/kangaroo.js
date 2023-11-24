44e6ce7cc1d906ee955858e4009fd23071ac22f9
Manideep4444 <manideepg200@gmail.com>
2023-11-23 23:41:25 -0600

    part 5 and part 6

--- models/kangaroo.js
+++ models/kangaroo.js
@@ -2,7 +2,15 @@ const mongoose = require("mongoose")
 const kangarooSchema = mongoose.Schema({
     k_name: String,
     k_age: Number,
-    k_jumpheight: Number
+    //k_jumpheight: Number,
+    k_jumpheight: {
+        type: Number,
+        required: true,
+        min: 4,
+        max: 4444,
+    }
+       
 })
 module.exports = mongoose.model("kangaroo",
 kangarooSchema)
+
